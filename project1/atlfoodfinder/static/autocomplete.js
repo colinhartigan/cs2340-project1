3b1be3151f3d455511a92005ea41f63904cf07a1
colin <42125428+colinhartigan@users.noreply.github.com>
2024-09-10 20:34:42 -0400

    fix list

--- project1/atlfoodfinder/static/autocomplete.js
+++ project1/atlfoodfinder/static/autocomplete.js
@@ -55,7 +55,8 @@ async function makeAcRequest(input) {
         a.innerText = placePrediction.text.toString();
 
         // Create a new list element.
-        const li = document.createElement("div", { class: "list-group-item list-group-item-action" });
+        const li = document.createElement("li");
+        li.setAttribute("class", "list-group-item list-group-item-action");
 
         li.appendChild(a);
         results.appendChild(li);
@@ -68,7 +69,7 @@ async function onPlaceSelected(place) {
         fields: ["displayName", "formattedAddress"],
     });
 
-    let placeText = document.createTextNode(place.displayName + ": " + place.formattedAddress);
+    let placeText = document.createTextNode(place.displayName);
 
     results.replaceChildren(placeText);
     input.value = "";
